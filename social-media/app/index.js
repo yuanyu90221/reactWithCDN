38a88823cda35489f95f74f08fd99d53e13d78a8
yuanyu90221 <yuanyu90221@gmail.com>
2017-08-29 11:49:15 +0800

    update new

--- social-media/app/index.js
+++ social-media/app/index.js
@@ -1,10 +1,30 @@
+var person = {
+    name: 'Json',
+    location: 'Taiwan, Taipei',
+    occupation: {
+        title: 'Web developer',
+        employer: '@json'
+    },
+    photo: "https://avatars2.githubusercontent.com/u/7987905?v=4&u=65d717e74740ad2da06247e71c8ff8c31c163579&s=400",
+    updates: [
+        {
+            platform: 'twitter',
+            status: '測試'
+        },
+        {
+            platform: 'facebook',
+            status: 'Nice Jobs'
+        }
+    ] 
+}
 class Card extends React.Component {
+    
     render(){
         return (
             <div className="card">
-                <Photo />
-                <Bio />
-                <Updates />
+                <Photo photo={person.photo} />
+                <Bio name={person.name} location={person.location} occupation={person.occupation}/>
+                <Updates updates={person.updates}/>
             </div>
         );
     }
@@ -14,7 +34,7 @@ class Photo extends React.Component {
     render() {
         return (
             <div className="photo">
-                <img src="https://avatars2.githubusercontent.com/u/7987905?v=4&u=65d717e74740ad2da06247e71c8ff8c31c163579&s=400" alt="json"/>
+                <img src={this.props.photo} alt="json"/>
             </div>
         );
     }
@@ -24,10 +44,10 @@ class Bio extends React.Component {
     render() {
         return (
             <div className="bio">
-                <h1 className="name">Json</h1>
-                <h2 className="location">Taiwan, Taipei</h2>
+                <h1 className="name">{this.props.name}</h1>
+                <h2 className="location">{this.props.location}</h2>
                 <div className="occupation">
-                    <p>innovation</p>
+                    <p>{this.props.occupation.title} at {this.props.occupation.employer}</p>
                 </div>
             </div>
         );
@@ -35,12 +55,21 @@ class Bio extends React.Component {
 } 
 
 class Updates extends React.Component {
+    updates() {
+      
+       return this.props.updates.map(function(update, index){
+            return (
+                <li className={"update" + update.platform} key={index}>
+                    {update.status}
+                </li>
+            )
+       });
+    }
     render() {
         return (
             <div className="updates">
                 <ul>
-                    <li className="update">Update</li>
-                    <li className="update">Update</li>
+                    {this.updates()}
                 </ul>
             </div>
         );
--- social-media/build/index.js
+++ social-media/build/index.js
@@ -1,11 +1,28 @@
+var person = {
+    name: 'Json',
+    location: 'Taiwan, Taipei',
+    occupation: {
+        title: 'Web developer',
+        employer: '@json'
+    },
+    photo: "https://avatars2.githubusercontent.com/u/7987905?v=4&u=65d717e74740ad2da06247e71c8ff8c31c163579&s=400",
+    updates: [{
+        platform: 'twitter',
+        status: '測試'
+    }, {
+        platform: 'facebook',
+        status: 'Nice Jobs'
+    }]
+};
 class Card extends React.Component {
+
     render() {
         return React.createElement(
-            "div",
-            { className: "card" },
-            React.createElement(Photo, null),
-            React.createElement(Bio, null),
-            React.createElement(Updates, null)
+            'div',
+            { className: 'card' },
+            React.createElement(Photo, { photo: person.photo }),
+            React.createElement(Bio, { name: person.name, location: person.location, occupation: person.occupation }),
+            React.createElement(Updates, { updates: person.updates })
         );
     }
 }
@@ -13,9 +30,9 @@ class Card extends React.Component {
 class Photo extends React.Component {
     render() {
         return React.createElement(
-            "div",
-            { className: "photo" },
-            React.createElement("img", { src: "https://avatars2.githubusercontent.com/u/7987905?v=4&u=65d717e74740ad2da06247e71c8ff8c31c163579&s=400", alt: "json" })
+            'div',
+            { className: 'photo' },
+            React.createElement('img', { src: this.props.photo, alt: 'json' })
         );
     }
 }
@@ -23,25 +40,27 @@ class Photo extends React.Component {
 class Bio extends React.Component {
     render() {
         return React.createElement(
-            "div",
-            { className: "bio" },
+            'div',
+            { className: 'bio' },
             React.createElement(
-                "h1",
-                { className: "name" },
-                "Json"
+                'h1',
+                { className: 'name' },
+                this.props.name
             ),
             React.createElement(
-                "h2",
-                { className: "location" },
-                "Taiwan, Taipei"
+                'h2',
+                { className: 'location' },
+                this.props.location
             ),
             React.createElement(
-                "div",
-                { className: "occupation" },
+                'div',
+                { className: 'occupation' },
                 React.createElement(
-                    "p",
+                    'p',
                     null,
-                    "innovation"
+                    this.props.occupation.title,
+                    ' at ',
+                    this.props.occupation.employer
                 )
             )
         );
@@ -49,23 +68,24 @@ class Bio extends React.Component {
 }
 
 class Updates extends React.Component {
+    updates() {
+
+        return this.props.updates.map(function (update, index) {
+            return React.createElement(
+                'li',
+                { className: "update" + update.platform, key: index },
+                update.status
+            );
+        });
+    }
     render() {
         return React.createElement(
-            "div",
-            { className: "updates" },
+            'div',
+            { className: 'updates' },
             React.createElement(
-                "ul",
+                'ul',
                 null,
-                React.createElement(
-                    "li",
-                    { className: "update" },
-                    "Update"
-                ),
-                React.createElement(
-                    "li",
-                    { className: "update" },
-                    "Update"
-                )
+                this.updates()
             )
         );
     }
